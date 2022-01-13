4fced2f463cca4396e307e29c9f852cf95e89624
abhinav226 <76432426+abhinav226@users.noreply.github.com>
2022-01-13 19:36:05 +0530

    Add files via upload

---
+++ backend/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    //mongoURI: "mongodb+srv://admin:Jake@12345@cluster0-8lbwj.mongodb.net/timer?retryWrites=true&w=majority",
+    
+    mongoURI: "mongodb+srv://DB-admin:uqSKi5C_W-YR:Bk@merncluster.acuay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    
+    secretOrKey: "secret"
+  };
